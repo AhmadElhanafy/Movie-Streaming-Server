59828f8655ddfc36aea7c419e9a93a7eb29846bb
Ahmad <ahmad.taher.elhanafy@gmail.com>
2025-08-31 16:49:53 +0300

    Fixed Migrations

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -14,12 +14,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 // Enable Swagger only in development
 if (app.Environment.IsDevelopment())
 {
