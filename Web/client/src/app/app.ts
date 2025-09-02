4fd3b8ed3e192c7643d123a131b927e0d87523de
Ahmad <ahmad.taher.elhanafy@gmail.com>
2025-09-02 13:30:47 +0300

    Full-Stack Communication Test #6

--- Web/client/src/app/app.ts
+++ Web/client/src/app/app.ts
@@ -10,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('client');
+
+  responseMessage: string = 'Super secret message!';
+
+  constructor(private http: HttpClient) {}
+
+  callBackend() {
+    this.http.get<{ message: string }>('http://backend/api/ping')
+      .subscribe({
+        next: (res) => this.responseMessage = res.message,
+        error: () => this.responseMessage = 'Error contacting backend'
+      });
+  }
 }
